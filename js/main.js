45bf7b3bcf6b8843f5cf8104eca593ba70c3d0c0
Olatz Aranzabe <olatzaranzabe@gmail.com>
2018-07-11 17:48:43 +0200

    film images do

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 var searchButton = document.querySelector('.search-button');
 var filmName = document.querySelector('.film-name');
 var newFilm;
+
 var filmsList = document.querySelector('.films-list');
 function  showFilm(){
   fetch('http://api.tvmaze.com/search/shows?q=' +filmName.value)
@@ -13,15 +14,24 @@ function  showFilm(){
       for(var i = 0; i < json.length; i++){
       // creando li's
         newFilm = document.createElement('li');
+        newFilm.classList.add('listElement');
       // metiendo li's en la ul
         filmsList.appendChild(newFilm);
 
-console.log(newFilm);
-        var ourNewFilm = json[i];
-        newFilm.append(ourNewFilm);
-        console.log(ourNewFilm);
-      // img.src = json.message;
-        document.body.innerHTML += newFilm;}
+        var ourFilmImage = json[i].show.image.medium;
+        var ourFilmName = json[i].show.name;
+        console.log(json[i].show.name);
+
+      // creando etiqueta img
+        var image= document.createElement('img');
+        newFilm.appendChild(image);
+        image.setAttribute('src', ourFilmImage);
+      // creando etiqueta h2
+      var filmTitle = document.createElement('h2');
+      // newFilm.appendChild(filmTile);
+        // newFilm.append(ourNewFilm);
+      // metiendo image and name en cada li
+    }
     });
 }
 
