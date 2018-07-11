e28e3818d3d5bd0c1dd9fd5c2647e24d7b0e8ee7
Olatz Aranzabe <olatzaranzabe@gmail.com>
2018-07-11 16:41:21 +0200

    html done and js li's create

--- js/main.js
+++ js/main.js
@@ -2,11 +2,27 @@
 
 var searchButton = document.querySelector('.search-button');
 var filmName = document.querySelector('.film-name');
+var newFilm;
+var filmsList = document.querySelector('.films-list');
+function  showFilm(){
+  fetch('http://api.tvmaze.com/search/shows?q=' +filmName.value)
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(json){
+      for(var i = 0; i < json.length; i++){
+      // creando li's
+        newFilm = document.createElement('li');
+      // metiendo li's en la ul
+        filmsList.appendChild(newFilm);
 
-fetch('http://api.tvmaze.com/search/shows?q=girls')
-  .then(function(response){
-    return response.text();
-  })
-  .then(function(html){
-    document.body.innerHTML = html;
-  });
+console.log(newFilm);
+        var ourNewFilm = json[i];
+        newFilm.append(ourNewFilm);
+        console.log(ourNewFilm);
+      // img.src = json.message;
+        document.body.innerHTML += newFilm;}
+    });
+}
+
+  searchButton.addEventListener('click', showFilm);
