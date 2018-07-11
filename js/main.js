19ae8a818de73aaf46714d6932e5c96f83568614
Olatz Aranzabe <olatzaranzabe@gmail.com>
2018-07-11 15:32:14 +0200

    js conected

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-console.log('>> Ready :)');
+var searchButton = document.querySelector('.search-button');
+var filmName = document.querySelector('.film-name');
+
+fetch('http://api.tvmaze.com/search/shows?q=girls')
+  .then(function(response){
+    return response.text();
+  })
+  .then(function(html){
+    document.body.innerHTML = html;
+  });
